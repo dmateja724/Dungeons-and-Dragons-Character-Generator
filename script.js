e16e4995a5c2d37b54a22220e1b8a6207623757f
Derrick <dmateja724@gmail.com>
2018-05-03 18:39:29 -0500

    initial commit
    d&d character generator.
    
    -everything is almost set.
    -when rolling stats it doesnt follow and rules or guidelines at the moment. but numbers randomly generate and insert themselves into their appropriate spot.
    - gold function hasnt been written yet
    - possibly some other sort of validation needs to be added that was missed

---
+++ script.js
@@ -0,0 +1,198 @@
+"use strict";
+var $ = function(id) { 
+    return document.getElementById(id); 
+};
+var diceRoll = function(max) {
+    var random;
+    if (!isNaN(max)) {
+        random = Math.random();               //value >= 0.0 and < 1.0
+        random = Math.floor(random * max);   //value is an integer between 0 and max - 1
+        random = random + 1;                //value is an integer between 1 and max
+    }
+    return random;
+};
+
+
+
+var raceD = [];
+raceD["dwarves"] = "Short, stocky, stern, and strong. They have a connection to the earth and ofthen live in mountains or underground lands.<br><br> Stat modifiers: +2 Constitution, +2 Wisdom, -2 Charisma.";
+raceD["elves"] = "Tall, long-lived, aloof, and connected to nature. Elves live in harmony with the natural world around them.<br><br> Stat modifiers: +2 Dexterity, +2 Intelligence, –2 Constitution.";
+raceD["gnomes"] = "Mysterious, adventure-seeking, and strange. Gnomes are the smallest common race.<br><br> Stat modifiers: +2 Constitution, +2 Charisma, –2 Strength.";
+raceD["halfElf"] = "Solitary, long-lived, graceful, and hearty. Half-elves are few in number, and tend to be wanderers due to their lack of homeland.<br><br> Stat modifiers: +2 to one ability score.";
+raceD["halfOrc"] = "Independent, strong, and distrusted. Half-orcs considered monstrosities by common folk. They are tall and powerfully built.<br><br> Stat modifiers: +2 to one ability score.";
+raceD["halfling"] = "Optimistic, cheerful, curious, and small in stature. On average, only 3 ft (.9 m) tall, they are agile but physically weak.<br><br> Stat modifiers: +2 Dexterity, +2 Charisma, –2 Strength.";
+raceD["human"] = "Adaptive, ambitious, and well-balanced. Humans are the dominant race, and are diverse in appearance and culture.<br><br> Stat modifiers: +2 to one ability score.";
+
+var classD = [];
+classD["barbarian"] = "Uncivilized and strong. Considered brutal berserkers.";
+classD["bard"] = "Charismatic and crafty. Bards use skills and spells to help and harm.";
+classD["cleric"] = "A firm believer in a deity. Clerics heal, raise the dead, and direct the wrath of god.";
+classD["druid"] = "At one with nature. Druids cast spells, interact with animals, and change shapes.";
+classD["fighter"] = "Firm and brave. Fighters are capable with weapons and comfortable in armor.";
+classD["monk"] = "Martial masters. Monks train their minds and bodies for offense and defense.";
+classD["paladin"] = "Followers of what is good and just. Paladins are devoted knights.";
+classD["ranger"] = "Masters of woodcraft and wildlife. Rangers track and hunt foes.";
+classD["rogue"] = "Stealthy assassins. Rogues are cunning thieves and able scouts.";
+classD["sorcerer"] = "Natural born spellcasters. Sorcerers command strange, ancient energies.";
+classD["wizard"] = "Lifelong magic students. Years of study allow wizards to use awesome magic power.";
+
+var displayRace = function() {  
+     
+    if ($("dwarves").checked == true){
+        $("raceBox").innerHTML = raceD["dwarves"];
+    } else if ($("elves").checked == true){
+        $("raceBox").innerHTML = raceD["elves"];
+    } else if ($("gnomes").checked == true){
+        $("raceBox").innerHTML = raceD["gnomes"];
+    } else if ($("halfElf").checked == true){
+        $("raceBox").innerHTML = raceD["halfElf"];
+    } else if ($("halfOrc").checked == true){
+        $("raceBox").innerHTML = raceD["halfOrc"];
+    } else if ($("halfling").checked == true){
+        $("raceBox").innerHTML = raceD["halfling"];
+    } else if ($("human").checked == true){
+        $("raceBox").innerHTML = raceD["human"];
+    }
+    
+};
+
+var displayClass = function() {
+    
+    if ($("barbarian").checked == true){
+        $("classBox").innerHTML = classD["barbarian"];
+    } else if ($("bard").checked == true){
+        $("classBox").innerHTML = classD["bard"];
+    } else if ($("cleric").checked == true){
+        $("classBox").innerHTML = classD["cleric"];
+    } else if ($("druid").checked == true){
+        $("classBox").innerHTML = classD["druid"];
+    } else if ($("fighter").checked == true){
+        $("classBox").innerHTML = classD["fighter"];
+    } else if ($("monk").checked == true){
+        $("classBox").innerHTML = classD["monk"];
+    } else if ($("paladin").checked == true){
+        $("classBox").innerHTML = classD["paladin"];
+    } else if ($("ranger").checked == true){
+        $("classBox").innerHTML = classD["ranger"];
+    } else if ($("rogue").checked == true){
+        $("classBox").innerHTML = classD["rogue"];
+    } else if ($("sorcerer").checked == true){
+        $("classBox").innerHTML = classD["sorcerer"];
+    } else if ($("wizard").checked == true){
+        $("classBox").innerHTML = classD["wizard"];
+    }
+};
+
+var rollStats = function() {
+    var die1 = diceRoll(6);
+    var die2 = diceRoll(6);
+    var die3 = diceRoll(6);
+    var die4 = diceRoll(6);
+    
+    $("str").value = diceRoll(6)+diceRoll(6)+diceRoll(6)+diceRoll(6);
+    $("dex").value = diceRoll(6)+diceRoll(6)+diceRoll(6)+diceRoll(6);
+    $("cons").value = diceRoll(6)+diceRoll(6)+diceRoll(6)+diceRoll(6);
+    $("int").value = diceRoll(6)+diceRoll(6)+diceRoll(6)+diceRoll(6);
+    $("wis").value = diceRoll(6)+diceRoll(6)+diceRoll(6)+diceRoll(6);
+    $("cha").value = diceRoll(6)+diceRoll(6)+diceRoll(6)+diceRoll(6);
+}
+
+var validate = function() {
+    if($("name").value == ""){
+        $("nameL").innerHTML = "Name:".fontcolor("red");
+        alert("Please enter a name.");
+    }
+    var raceRadio = document.getElementsByName('race');
+    var raceValue = false;
+
+    for(var i=0; i<raceRadio.length;i++){
+        if(raceRadio[i].checked == true){
+            raceValue = true;
+        }
+    }
+    if(raceValue == false){
+        $("raceBox").innerHTML = "-------------------------------------------------- Please Select A Race --------------------------------------------------".fontcolor("red");
+        alert("Please select a Race.");
+    }
+    
+    var classRadio = document.getElementsByName('class');
+    var classValue = false;
+
+    for(var j=0; j<classRadio.length;j++){
+        if(classRadio[j].checked == true){
+            classValue = true;
+        }
+    }
+    if(classValue == false){
+        $("classBox").innerHTML = "-------------------------------------------------- Please Select A Class --------------------------------------------------".fontcolor("red");
+        alert("Please Select a Class.");
+    }
+
+    if (raceValue == true && classValue == true && $("name").value !== ""){
+        var conf = confirm("Does everything look good? Click [OK] to roll stats or [CANCEL] to make a change.");
+        if (conf == true){
+            rollStats();
+        } 
+    }
+    
+};
+
+var reset = function() {
+    
+    $("name").value = "";
+    $("dwarves").checked = false;
+    $("elves").checked = false;
+    $("gnomes").checked = false;
+    $("halfElf").checked = false;
+    $("halfOrc").checked = false;
+    $("halfling").checked = false;
+    $("human").checked = false;
+    $("barbarian").checked = false;
+    $("bard").checked = false;
+    $("cleric").checked = false;
+    $("druid").checked = false;
+    $("fighter").checked = false;
+    $("monk").checked = false;
+    $("paladin").checked = false;
+    $("ranger").checked = false;
+    $("rogue").checked = false;
+    $("sorcerer").checked = false;
+    $("wizard").checked = false;
+    $("raceBox").innerHTML = "-------------------------------------------------- Please Select A Race --------------------------------------------------";
+    $("classBox").innerHTML = "-------------------------------------------------- Please Select A Class --------------------------------------------------";
+    $("name").focus();
+    $("str").value = "";
+    $("dex").value = "";
+    $("cons").value = "";
+    $("int").value = "";
+    $("wis").value = "";
+    $("cha").value = "";
+
+    
+};
+
+window.onload = function() {
+    
+    $("name").focus();
+    $("dwarves").onclick = displayRace;
+    $("elves").onclick = displayRace;
+    $("gnomes").onclick = displayRace;
+    $("halfElf").onclick = displayRace;
+    $("halfOrc").onclick = displayRace;
+    $("halfling").onclick = displayRace;
+    $("human").onclick = displayRace;
+    $("barbarian").onclick = displayClass;
+    $("bard").onclick = displayClass;
+    $("cleric").onclick = displayClass;
+    $("druid").onclick = displayClass;
+    $("fighter").onclick = displayClass;
+    $("monk").onclick = displayClass;
+    $("paladin").onclick = displayClass;
+    $("ranger").onclick = displayClass;
+    $("rogue").onclick = displayClass;
+    $("sorcerer").onclick = displayClass;
+    $("wizard").onclick = displayClass;
+    $("generate").onclick = validate;
+    $("reset").onclick = reset;
+    
+};
